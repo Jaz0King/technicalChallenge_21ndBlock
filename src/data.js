ce0dedf968433bca94c041cf571bef81bb69a676
Jazito <jazo@Jazito-Air.local>
2022-01-12 05:22:42 -0600

    pintar tabla dinámicamente

--- src/data.js
+++ src/data.js
@@ -1,9 +1,12 @@
+import {listaP} from "./index.js"
+
 //Se declaran los arreglos para añadir los registros 
-let arrProp =[]
+let arrProp = []
+let dataRegistro = []
 
 var db = firebase.firestore();
 
-var onGetData = (callback) => db.collection(ListaP).onSnapshot(callback)
+var onGetData = (callback) => db.collection(listaP).onSnapshot(callback)
 
 export async function traerDatos() {
     onGetData((querySnapshot) => {
@@ -21,43 +24,47 @@ export async function traerDatos() {
     })
   }
 
-// Funcionalidad para pintar dinámicamente la Lista de propósitos
-function pintarProp() {
-    //console.log(arrProp.length);
-
-    for (let prop of arrProp) {
-        document.getElementById("contTable").innerHTML += `
-                <div class="card" style="width: 18rem;">
-                 <h4 class="card-header" value="${prop}" data-bs-target="#staticBackdrop${prop.id}"> ${prop.nombre} ${prop.fecha} </h4>
-                  <div class="card-body">
-                   <!-- Button trigger modal -->
-                   <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop${prop.id}">
-                     Ver Mas
-                   </button>
-                   
-                   <!-- Modal -->
-                   <div class="modal fade" id="staticBackdrop${prop.id}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                     <div class="modal-dialog">
-                       <div class="modal-content">
-                         <div class="modal-header">
-                           <h5 class="modal-title"  id="staticBackdropLabel"> ${prop.nombre} </h5>
-                         </div>
-                         <div class="modal-body">
-                         <ul class="list-group list-group-flush">
-                        <li class="list-group-item"><b>Compañía:</b> ${prop.descripcion}</li>
-                        <li class="list-group-item"><b>Motivo:</b> ${prop.fecha}</li>
-                        </ul>
-                         </div>
-                         <div class="modal-footer">
-                           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                           <button type="button" class="btn btn-primary">Check-Out</button>
-                         </div>
-                       </div>
-                     </div>
-                   </div>   
-                   </div>
-                </div> ` 
-      }
+  function pintarProp() {
+    document.getElementById('contTable').innerHTML = " "; 
+    
+      console.log(arrProp.length)
+       for (let prop of arrProp) {
+           document.getElementById("contTable").innerHTML += `
+                    <tbody>
+                      <tr>
+                        <th scope="row">1</th>
+                        <td value="${prop}" data-bs-target="#staticBackdrop${prop.id}"> ${prop.nombre} </td>
+                        <td> 
+                          <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop${prop.id}">
+                            Ver Mas
+                          </button> 
+                        </td>
+                      </tr>
+                    </tbody>
+                                         
+                      <!-- Modal -->
+                      <div class="modal fade" id="staticBackdrop${prop.id}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                          <div class="modal-content">
+                            <div class="modal-header">
+                              <h5 class="modal-title"  id="staticBackdropLabel"> ${prop.nombre} </h5>
+                            </div>
+                            <div class="modal-body">
+                            <ul class="list-group list-group-flush">
+                           <li class="list-group-item"><b>Descripción:</b> ${prop.descripcion} </li>
+                           </ul>
+                            </div>
+                            <div class="modal-footer">
+                              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                              <button type="button" class="btn btn-primary">Cumplido</button>
+                            </div>
+                          </div>
+                        </div>
+                      </div>   
+                      </div>
+                   </div> ` 
+        }
+        
     }
 
    
--- src/index.js
+++ src/index.js
@@ -2,40 +2,15 @@
 // Botones de regreso a listas de propósitos y salida 
 var btnListas = document.getElementById('btnListas')
 var btnReturn = document.getElementById('btnReturn')
+let limpiarP = document.getElementById('btnCrear')
 var listaP = localStorage['objectToPass']
-
 var db = firebase.firestore();
-
-
-var crearProp = document.getElementById('btnCrear');
-crearProp.addEventListener('click', async (e)=> {
-  e.preventDefault();
-  const obj = {
-        nombre: document.getElementById('nombreProp').value,
-        descripcion: document.getElementById('descProp').value
-        //fecha: document.getElementById('fecha').value,
-        //motivacion: document.getElementById('motivacion').value
-    
-}
-
-traerDatos()
-  obj.lista = listaP;
-  //console.log(obj)
-  await saveObj(obj);
-  alert("PROPÓSITO AÑADIDO");
-      //--- window.location.href = "./index.html";
-})
-
-const saveObj = (obj) => {
-  db.collection(listaP).doc().set(obj);
-  console.log(obj)
-}
-
 //Se declaran los arreglos para añadir los registros 
 let arrProp = []
 let dataRegistro = []
 
-var onGetData = (callback) => db.collection(listaP).onSnapshot(callback)
+let onGetData = (callback) => db.collection(listaP).onSnapshot(callback)
+
 
 async function traerDatos() {
     onGetData((querySnapshot) => {
@@ -44,91 +19,126 @@ async function traerDatos() {
           nuevoProp.id = doc.id
        //console.log(nuevoProp)
         dataRegistro.push(nuevoProp)
+        
       })
-             //se pasa la data de firebasea una variable 
+       //se pasa la data de firebasea una variable 
       arrProp = dataRegistro
       console.log(arrProp)
       pintarProp()
-  
+      
     })
+}
+
+var crearProp = document.getElementById('btnCrear')
+crearProp.addEventListener('click', async (e)=> {
+  e.preventDefault();
+  const obj = {
+        nombre: document.getElementById('nombreProp').value,
+        descripcion: document.getElementById('descProp').value
+        
   }
+  
+  traerDatos()  
+  obj.lista = listaP;
+  //console.log(obj)
+  await saveObj(obj);
+  alert('PROPÓSITO AÑADIDO')
+  window.location.href = "./index.html"
+})
+
+const saveObj = (obj) => {
+  db.collection(listaP).doc().set(obj);
+  console.log(obj) 
+  
+}
+
 
 // Funcionalidad para pintar dinámicamente la Lista de propósitos
-function pintarProp() {
-    console.log(arrProp.length);
-
-    for (let prop of arrProp) {
-        document.getElementById("contTable").innerHTML += `
-        <table class="table table-dark table-hover">
-                        <thead>
-                          <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Nombre</th>
-                            <th scope="col">Descripción</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          <tr>
-                            <th scope="row">1</th>
-                            <td value="${prop}" data-bs-target="#staticBackdrop${prop.id}"> ${prop.nombre} ${prop.fecha} </td>
-                            <td> <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop${prop.id}">
-                            Ver Mas
-                          </button> 
-                          </td>
-                          </tr>
-                        </tbody>
-                      </table> 
-                                      
-                   <!-- Modal -->
-                   <div class="modal fade" id="staticBackdrop${prop.id}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                     <div class="modal-dialog">
-                       <div class="modal-content">
-                         <div class="modal-header">
-                           <h5 class="modal-title"  id="staticBackdropLabel"> ${prop.nombre} </h5>
-                         </div>
-                         <div class="modal-body">
-                         <ul class="list-group list-group-flush">
-                        <li class="list-group-item"><b>Compañía:</b> ${prop.descripcion}</li>
-                        <li class="list-group-item"><b>Motivo:</b> ${prop.fecha}</li>
-                        </ul>
-                         </div>
-                         <div class="modal-footer">
-                           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                           <button type="button" class="btn btn-primary">Check-Out</button>
-                         </div>
-                       </div>
-                     </div>
-                   </div>   
-                   </div>
-                </div> ` 
+let pintarProp = () => {
+  document.getElementById('contTable').innerHTML = " "; 
+    console.log(arrProp.length)
+     for (let prop of arrProp) {
+         document.getElementById("contTable").innerHTML += `
+                  <tbody>
+                    <tr>
+                      <th scope="row">1</th>
+                      <td value="${prop}" data-bs-target="#staticBackdrop${prop.id}"> ${prop.nombre} </td>
+                      <td> 
+                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop${prop.id}">
+                          Ver Mas
+                        </button> 
+                      </td>
+                    </tr>
+                  </tbody>
+                                       
+                    <!-- Modal -->
+                    <div class="modal fade" id="staticBackdrop${prop.id}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                      <div class="modal-dialog">
+                        <div class="modal-content">
+                          <div class="modal-header">
+                            <h5 class="modal-title"  id="staticBackdropLabel"> ${prop.nombre} </h5>
+                          </div>
+                          <div class="modal-body">
+                          <ul class="list-group list-group-flush">
+                         <li class="list-group-item"><b>Descripción:</b> ${prop.descripcion} </li>
+                         </ul>
+                          </div>
+                          <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Cumplido</button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>   
+                    </div>
+                 </div> ` 
       }
-    }
+      
+  }
+ 
+  document.addEventListener('DOMContentLoaded',traerDatos,false)
+  
+  btnCrear.addEventListener('click', () =>{
+    //window.location.href = "./index.html"
+    
+  })
 
-//Pinta dinamicamente el nombre de la lista de propósitos
+  //Pinta dinamicamente el nombre de la lista de propósitos
 if (listaP == 'personal') {
-    let titleP = document.getElementById('tipoLista')
-    titleP.innerHTML = `<h2> PERSONALES </h2>`
+  let titleP = document.getElementById('tipoLista')
+  titleP.innerHTML = `<h2> PERSONALES </h2>` 
+} else {
+  if (listaP == 'profesional') {
+      let titleP = document.getElementById('tipoLista')
+      titleP.innerHTML = `<h2> PROFESIONALES </h2>`
+      
   } else {
-    if (listaP == 'profesional') {
+    if (listaP == 'relaciones') {
+      let titleP = document.getElementById('tipoLista')
+      titleP.innerHTML = `<h2> RELACIONES </h2>`
+    }  else {
+      if (listaP == 'familia') {
+       
         let titleP = document.getElementById('tipoLista')
-        titleP.innerHTML = `<h2> PROFESIONALES </h2>`
-    } else {
-      if (listaP == 'relaciones') {
-        let titleP = document.getElementById('tipoLista')
-        titleP.innerHTML = `<h2> RELACIONES </h2>`
-      }  else {
-        if (listaP == 'familia') {
-          let titleP = document.getElementById('tipoLista')
-          titleP.innerHTML = `<h2> FAMILIARES </h2>`
-        } else {
-            if (listaP == 'salud') {
-              let titleP = document.getElementById('tipoLista')
-              titleP.innerHTML = `<h2> SALUD Y APRENDIZAJE </h2>`
-            }
+        titleP.innerHTML = `<h2> FAMILIARES </h2>`
+      } else {
+          if (listaP == 'salud') {
+            let titleP = document.getElementById('tipoLista')
+            titleP.innerHTML = `<h2> SALUD Y APRENDIZAJE </h2>`
           }
-      }
+        }
     }
   }
+}
+
+ 
+
+
+
+
+
+  
+
 
 btnReturn.addEventListener('click', () => {
     window.location.href = "./login.html"
