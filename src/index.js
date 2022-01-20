b2cac904d43f691573c8bf6078a94b4dbab14ae0
Jazito <jazo@Jazito-Air.local>
2022-01-20 17:28:25 -0600

    refactorización for Each

--- src/index.js
+++ src/index.js
@@ -1,18 +1,86 @@
-import {traerDatos} from "./modules/data.js"
-traerDatos()
-//document.addEventListener('DOMContentLoaded',traerDatos,false)
-//Función para activar el calendario del modal
+import {saveTask, getTask, onGetTask} from './modules/data.js';
+const taskForm = document.getElementById("task-form")
+const taskCont = document.getElementById('task-container')
+
+//Evento para arrancar la app al cargar la página
+window.addEventListener('DOMContentLoaded', async () => {
+    onGetTask((querySnapshot)=> {
+
+    let html = ''
+    //Mostrar las tareas dinámicamente
+    querySnapshot.forEach(doc =>{
+        const prop = doc.data()
+        prop.id = doc.id
+        html += `
+        <tbody id="tab">
+        <tr id="tab1">
+          <th scope="row" id="enu">${prop.fechaIni} </th>
+          <td value="${prop}" data-bs-target="#staticBackdrop${prop.id}"> ${prop.nombre} </td>
+          <td> 
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop${prop.id}">
+              Ver Mas
+            </button> 
+          </td>
+        </tr>
+      </tbody>
+                           
+        <!-- Modal -->
+        <div class="modal fade" id="staticBackdrop${prop.id}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+              <h3 class="modal-title" id="exampleModalLabel">${prop.nombre} </h3>
+              </div>
+              <div class="modal-body">
+              <ul class="list-group list-group-flush">
+             <li class="list-group-item"><b>Descripción:</b> ${prop.description} </li>
+             <li class="list-group-item"><b>Fecha de Inicio:</b> ${prop.fechaIni} </li>
+             <li class="list-group-item"><b>Fecha límite:</b> ${prop.fechaFin} </li>
+             </ul>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary" id="done">Cumplido</button>
+              </div>
+            </div>
+          </div>
+        </div>   
+        </div>
+     </div> `    
+        
+    })
+
+    taskCont.innerHTML = html
+})
+})
+
+// Función para guardar los datos ingresados 
+taskForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const title = taskForm['title']
+    const description = taskForm['descrip']
+    const fechaIni = taskForm['fechaProp']
+    //const fechaFin = taskForm['fechaPropCard']
+
+    saveTask(title.value, description.value, fechaIni.value)
+
+    taskForm.reset()
+
+})
+
+// Función para llamar el calendario
 $(function() {
-  $('#datepicker').datepicker();
-});
-$(function() {
-  $('#datepicker2').datepicker();
-});
+    $('#datepicker').datepicker();
+  });
+  $(function() {
+    $('#datepicker2').datepicker();
+  });
 
 // Botones de regreso a listas de propósitos y salida 
 const btnListas = document.getElementById('btnListas')
 const btnReturn = document.getElementById('btnReturn')
-const listaP = localStorage['objectToPass']
+export const listaP = localStorage['objectToPass']
  
 
   //Pinta dinamicamente el nombre de la lista de propósitos
@@ -50,7 +118,4 @@ btnReturn.addEventListener('click', () => {
 
 btnListas.addEventListener('click', () => {
     window.location.href = "./listas.html"
-  })
-
-  
-
+  })  
--- src/modules/data.js
+++ src/modules/data.js
@@ -1,122 +1,28 @@
-const listaP = localStorage['objectToPass']             //Variable para traer los datos de la pantalla listas 
-const db = firebase.firestore();                         //Variable para traer los datos de la base de datos 
-const crearProp = document.getElementById('btnCrear') 
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.3/firebase-app.js";
+  import { getFirestore, 
+           collection, 
+           addDoc, 
+           getDocs,
+           onSnapshot } from "https://www.gstatic.com/firebasejs/9.6.3/firebase-firestore.js";
+  import { listaP } from "../index.js"        
 
-//Se declaran los arreglos para añadir los registros    
-let arrProp = []       
-let dataRegistro = []
+  const firebaseConfig = {
+    apiKey: "AIzaSyC578jfsE2ia0sleabnRua4kbYlBtcDOtI",
+    authDomain: "tareas-7f3ee.firebaseapp.com",
+    projectId: "tareas-7f3ee",
+    storageBucket: "tareas-7f3ee.appspot.com",
+    messagingSenderId: "620012180202",
+    appId: "1:620012180202:web:b63b743a0459ef40ecfd93"
+  };
 
-//Variable para traer la data de firebase
-const onGetData = (callback) => db.collection(listaP).onSnapshot(callback)
-
-//Función para capturar los datos 
-crearProp.addEventListener('click', async (e)=> {
- e.preventDefault();
- const obj = {
-       nombre: document.getElementById('nombreProp').value,
-       descripcion: document.getElementById('descProp').value,
-       fecha: document.getElementById('fechaProp').value,
-       fechaFin: document.getElementById('fechaPropCad').value,
- }
-
- traerDatos()  
- obj.lista = listaP;
- 
- //console.log(obj)
- await saveObj(obj);
-
- setTimeout(() => {
-  alert('¡PROPÓSITO AÑADIDO, AHORA ES UN RETO!')
-  window.location.href = "./index.html"
- }, 500)
- 
-})
-
-export const saveObj = (obj) => {
- db.collection(listaP).doc().set(obj);
- console.log(obj) 
-}
-
-
-export async function traerDatos() {
-  onGetData((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-       let nuevoProp = doc.data()
-        nuevoProp.id = doc.id
-     //console.log(nuevoProp)
-      dataRegistro.push(nuevoProp)
-      
-    })
-     //se pasa la data de firebasea una variable 
-    arrProp = dataRegistro
-    //console.log(arrProp)
-    pintarProp()
-  })
-}
-
-// Funcionalidad para pintar dinámicamente el propósito la Lista de propósitos
-const pintarProp = () => {
-  
-  document.getElementById('contTable').innerHTML = " "; 
-   // console.log(arrProp.length)
-  
-     for (let prop of arrProp) {
-         document.getElementById("contTable").innerHTML += `
-                  <tbody id="tab">
-                    <tr id="tab1">
-                      <th scope="row" id="enu">${prop.fecha} </th>
-                      <td value="${prop}" data-bs-target="#staticBackdrop${prop.id}"> ${prop.nombre} </td>
-                      <td> 
-                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop${prop.id}">
-                          Ver Mas
-                        </button> 
-                      </td>
-                    </tr>
-                  </tbody>
-                                       
-                    <!-- Modal -->
-                    <div class="modal fade" id="staticBackdrop${prop.id}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                      <div class="modal-dialog">
-                        <div class="modal-content">
-                          <div class="modal-header">
-                          <h3 class="modal-title" id="exampleModalLabel">${prop.nombre} </h3>
-                          </div>
-                          <div class="modal-body">
-                          <ul class="list-group list-group-flush">
-                         <li class="list-group-item"><b>Descripción:</b> ${prop.descripcion} </li>
-                         <li class="list-group-item"><b>Fecha de Inicio:</b> ${prop.fecha} </li>
-                         <li class="list-group-item"><b>Fecha límite:</b> ${prop.fechaFin} </li>
-                         </ul>
-                          </div>
-                          <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary" id="done">Cumplido</button>
-                          </div>
-                        </div>
-                      </div>
-                    </div>   
-                    </div>
-                 </div> ` 
-      } 
-      const logrado = document.getElementById('done')
-      logrado.addEventListener('click', () =>{
-        
-        logrado.innerHTML = `
-        <audio autoplay><source src="../assets/audio/avengers.mp3" type="video/mp4"></audio>
-        `
-        alert("¡FELICIDADES! Estoy muy orgulloso de tí, sabía que lo lograrías.")
-        let bgc = document.getElementById('tab1')
-        bgc.style.color = "#dc3545"
-        
-      })
-      
-  }
-
-   
-  
-  
-
-
-
-
-  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  // Initialize Firestore
+  const db = getFirestore();
+  // Funcion para agregar datos a firebase
+  export const saveTask = (nombre,description, fechaIni) => addDoc(collection(db,listaP), {nombre, description, fechaIni})
+  // Funcion para traer Datos de firebase
+  export const getTask = () => getDocs(collection(db,listaP))
+  // Funcion para actualizar los datos cuando cambien
+  export const onGetTask = (callback) => onSnapshot(collection(db,listaP),callback)
